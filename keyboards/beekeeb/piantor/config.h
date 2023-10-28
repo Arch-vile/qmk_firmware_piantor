a3cf66dc068216df2a20aab25d182758c7c23213
Mikko Ravimo <mikko.ravimo@futurice.com>
2023-10-28 15:47:04 +0300

    use PERMISSIVE HOLD for better shift usage

--- keyboards/beekeeb/piantor/config.h
+++ keyboards/beekeeb/piantor/config.h
@@ -12,3 +12,8 @@
 #define SERIAL_USART_RX_PIN GP1
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// TAPPING_TERM is also used by the tap dance. So though we could set it long, because permissive hold,
+// the tap dance would feel sluggish as it would wait long time for the second tap.
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
