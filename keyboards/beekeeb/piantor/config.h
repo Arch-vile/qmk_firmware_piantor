4f9a50d3eeaa17734c2de53ffdf556c3d396050a
mravimo <mikko.ravimo@futurice.com>
2023-11-01 17:40:14 +0200

    add docs

--- keyboards/beekeeb/piantor/config.h
+++ keyboards/beekeeb/piantor/config.h
@@ -16,4 +16,7 @@
 // TAPPING_TERM is also used by the tap dance. So though we could set it long, because permissive hold,
 // the tap dance would feel sluggish as it would wait long time for the second tap.
 #define TAPPING_TERM 200
+// This is a must have setting for getting home row mods to work reliably
+// With LSFT_T(KC_F) given the sequence executed faster then TAPPING_TERM: F down, J down, J up, F up
+// would emit 'j' instead of 'J'
 #define PERMISSIVE_HOLD
