4334374e9844d7c17273ef1bc014e46c426e8c89
Arch-vile <mikko.ravimo@gmail.com>
2024-01-16 09:39:12 +0200

    conventional arrows

--- keyboards/beekeeb/piantor/keymaps/Arch-vile/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/Arch-vile/keymap.c
@@ -133,10 +133,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           KC_NO,      KC_BSPC,      KC_NO,     /**/     KC_NO,      KC_NO,      KC_NO
     ),
     [NAV] = LAYOUT_split_3x6_3(
-        KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,            LGUI(KC_X),         KC_TRNS,        /**/    KC_TRNS,        KC_PGDN,    KC_PGUP,    KC_TRNS,        KC_TRNS,        KC_TRNS,
-        KC_TRNS,    LGUI(KC_A), LGUI(KC_RIGHT), LALT(KC_RIGHT),     KC_RIGHT,           SGUI(KC_Z),     /**/    LGUI(KC_Z),     KC_UP,      KC_END,     LGUI(KC_C),     LGUI(KC_V),     KC_TRNS,
-        KC_TRNS,    KC_TRNS,    LGUI(KC_LEFT),  LALT(KC_LEFT),      KC_LEFT,            KC_TRNS,        /**/    KC_TRNS,        KC_DOWN,    KC_HOME,    KC_TRNS,        KC_TRNS,        KC_TRNS,
-                                                KC_LSFT,            KC_LCTL,            KC_LALT,        /**/    KC_TRNS,        KC_TRNS,    KC_TRNS
+        KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,            KC_TRNS,            KC_TRNS,        /**/    KC_TRNS,        KC_PGUP,        KC_UP,    KC_TRNS,        KC_TRNS,        KC_TRNS,
+        KC_TRNS,    KC_LCTL,    KC_LALT,        KC_LGUI,            KC_LSFT,            KC_TRNS,        /**/    KC_HOME,        KC_LEFT,        KC_DOWN,  KC_RIGHT,       KC_END,         KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,            KC_TRNS,            KC_TRNS,        /**/    KC_TRNS,        KC_PGDN,        KC_TRNS,  KC_TRNS,        KC_TRNS,        KC_TRNS,
+                                                KC_TRNS,            KC_TRNS,            KC_TRNS,        /**/    SGUI(KC_LEFT),  LSA(KC_LEFT),   KC_TRNS
     ),
     [MOUSE] = LAYOUT_split_3x6_3(
         KC_TRNS,    KC_TRNS,            KC_TRNS,                KC_TRNS,            KC_TRNS,            KC_TRNS,           /**/     KC_TRNS,    KC_TRNS,             KC_TRNS,                KC_TRNS,                KC_TRNS,            KC_TRNS,
