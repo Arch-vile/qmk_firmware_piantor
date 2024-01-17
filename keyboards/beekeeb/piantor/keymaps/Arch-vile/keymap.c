f0aeadf470931ba8d0bc175c65f3296f51538f44
Arch-vile <mikko.ravimo@gmail.com>
2024-01-17 11:25:19 +0200

    relocate screenshots

--- keyboards/beekeeb/piantor/keymaps/Arch-vile/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/Arch-vile/keymap.c
@@ -145,10 +145,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 KC_TRNS,            KC_TRNS,            KC_TRNS,           /**/     KC_TRNS,    KC_TRNS,             KC_TRNS
     ),
     [WIN] = LAYOUT_split_3x6_3(
-        KC_NO, KC_NO,           KC_NO,      KC_NO,            KC_NO,           SGUI(KC_5),             /**/   SGUI(KC_4),           KC_NO,          KC_NO,          KC_NO,      KC_NO,  KC_NO,
-        KC_NO, LGUI(KC_GRV),    KC_NO,      LGUI(KC_RCBR),    CMD_TAB,         KC_NO,             /**/   LGUI(KC_PLUS),   LCAG(KC_DOWN),  LCAG(KC_UP),    KC_NO,      KC_NO,  KC_NO,
-        KC_NO, KC_NO,           KC_NO,      LGUI(KC_LCBR),    LSFT_T(CMD_TAB), KC_NO,             /**/   LGUI(KC_MINS),   KC_NO,          KC_NO,          KC_NO,      KC_NO,  KC_NO,
-                                             KC_NO,           KC_NO,           KC_NO,             /**/   LCAG(KC_LEFT),   LCAG(KC_ENT),   LCAG(KC_RIGHT)
+        KC_NO, KC_NO,           KC_NO,      KC_NO,            KC_NO,           SGUI(KC_5),              /**/   KC_TRNS,                   KC_NO,          KC_NO,          KC_NO,      KC_NO,  KC_NO,
+        KC_NO, LGUI(KC_GRV),    KC_NO,      LGUI(KC_RCBR),    CMD_TAB,         LSFT(LGUI(LCTL(KC_4))),  /**/   LGUI(KC_PLUS),             LCAG(KC_DOWN),  LCAG(KC_UP),    KC_NO,      KC_NO,  KC_NO,
+        KC_NO, KC_NO,           KC_NO,      LGUI(KC_LCBR),    LSFT_T(CMD_TAB), SGUI(KC_4),              /**/   LGUI(KC_MINS),             KC_NO,          KC_NO,          KC_NO,      KC_NO,  KC_NO,
+                                             KC_NO,           KC_NO,           KC_NO,                   /**/   LCAG(KC_LEFT),             LCAG(KC_ENT),   LCAG(KC_RIGHT)
     ),
     [NUM] = LAYOUT_split_3x6_3(
         KC_NO, KC_NO,   KC_F1,  KC_F2,  KC_F3,  KC_F11,    /**/        KC_NO,   KC_1,   KC_2,   KC_3,   KC_NO,  KC_NO,
